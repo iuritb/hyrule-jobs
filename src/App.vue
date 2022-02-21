07e30453f74dd55336d0fab189e9d95b8f454788
iuri.barros <iuri.t.barros@gmail.com>
2022-02-21 17:20:48 -0300

    Functions

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="app">
-    <p>
-      <JobList :jobs="jobs"/>
-    </p>
+    <header>
+      <div class="order">
+        <button @click="handleClick('title')">order by title</button>
+        <button @click="handleClick('salary')">order by salary</button>
+        <button @click="handleClick('location')">order by location</button>
+      </div>
+    </header>
+    <JobList :jobs="jobs" :order="order" />
   </div>
 </template>
 
@@ -10,6 +15,7 @@
 import { defineComponent, reactive, ref, toRefs } from "vue";
 import JobList from "./components/JobsList.vue";
 import Job from "./types/Jobs";
+import OrderTerm from "./types/OrderTerm";
 
 export default defineComponent({
   name: "App",
@@ -37,11 +43,33 @@ export default defineComponent({
         id: "5",
       },
     ]);
+    const order = ref<OrderTerm>("title");
 
-    return { jobs };
+    const handleClick = (term: OrderTerm) => {
+      order.value = term;
+    };
+
+    return { jobs, order, handleClick };
   },
 });
 </script>
 
+
 <style>
+header {
+  text-align: center;
+}
+header .order {
+  margin-top: 20px;
+}
+button {
+  margin: 0 10px;
+  color: #1195c9;
+  border: 3px solid #1195c9;
+  background: #d5f0ff;
+  padding: 8px 16px;
+  border-radius: 4px;
+  cursor: pointer;
+  font-weight: bold;
+}
 </style>
--- src/components/JobsList.vue
+++ src/components/JobsList.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="job-list">
+    <p>Ordered by {{ order }}</p>
     <ul>
       <li v-for="job in jobs" :key="job.id">
         <h2>{{ job.title }} in {{ job.location }}</h2>
@@ -20,14 +21,19 @@
 </template>
 <script lang="ts">
 import Job from "@/types/Jobs";
+import OrderTerm from "@/types/OrderTerm";
 import { defineComponent, PropType } from "vue";
 
 export default defineComponent({
   props: {
     jobs: {
-      required: true,
       type: Array as PropType<Job[]>,
+      required: true
     },
+     order: {
+      type: String as PropType<OrderTerm>,
+      required: true
+    }
   },
 });
 </script>
